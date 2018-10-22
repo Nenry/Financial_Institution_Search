13af0e81e4b3fa115858effe2659c6b1d1530e42
HN <hhnguyen92@gmail.com>
2018-10-22 14:02:21 -0700

    Implement filter feature by term and type

--- frontend/autocomplete.jsx
+++ frontend/autocomplete.jsx
@@ -14,10 +14,32 @@ class AutoComplete extends React.Component {
   }
 
   matches() {
+        if (this.state.inputType.length === 0 && this.state.inputTerm.length === 0) {
+          return jsonFile.products;
+
+        } else if (this.state.inputType.length === 0) {
+          return (
+
+            jsonFile.products.filter((obj) =>
+            obj.name.toLowerCase().includes(this.state.inputTerm.toLowerCase())
+            )
+          );
+
+        } else {
+          return (
+
+            jsonFile.products.filter((obj) =>
+            obj.name.toLowerCase().includes(this.state.inputTerm.toLowerCase()) &&
+            obj.type === this.state.inputType
+            )
+          );
+          }
+  }
+        
+    
     //match names
     //match type
     //
-  }
 
   render() {
     return (
@@ -27,12 +49,13 @@ class AutoComplete extends React.Component {
           <label>Type:</label>
         <select onChange={(e) => this.setState({inputType: e.target.value})}>
           <option value=''>All</option>
-          <option value='CREDIT_CARD'>Credit Card</option>
           <option value='BANK'>Bank</option>
+          <option value='CREDIT_CARD'>Credit Card</option>
           <option value='INVESTMENT'>Investment</option>
+          <option value='LOAN'>Loan</option>
         </select>
         <ul>
-          {jsonFile.products.map((entity, idx) => 
+          {this.matches().map((entity, idx) => 
             <li key={idx}>
               {entity.name}
             </li>
