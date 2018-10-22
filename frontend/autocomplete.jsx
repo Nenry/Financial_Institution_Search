1e172f674d3da094c53bc6f7c1ff0831472dc530
HN <hhnguyen92@gmail.com>
2018-10-22 15:50:58 -0700

    Modify match function to take out duplicates

--- frontend/autocomplete.jsx
+++ frontend/autocomplete.jsx
@@ -14,26 +14,43 @@ class AutoComplete extends React.Component {
   }
 
   matches() {
+
+      const results = {};
         if (this.state.inputType.length === 0 && this.state.inputTerm.length === 0) {
           return; 
 
         } else if (this.state.inputType.length === 0) {
-          return (
 
-            jsonFile.products.filter((obj) =>
-            obj.name.toLowerCase().includes(this.state.inputTerm.toLowerCase())
-            )
-          );
+
+          jsonFile.products.forEach((obj) => {
+            if (obj.name.toLowerCase().includes(this.state.inputTerm.toLowerCase())) {
+              results[obj.name] = obj.url;
+            }
+          });
+          // return (
+
+          //   jsonFile.products.filter((obj) =>
+          //   obj.name.toLowerCase().includes(this.state.inputTerm.toLowerCase())
+          //   )
+          // );
 
         } else if (this.state.inputType.length > 0 && this.state.inputTerm.length > 0) {
-          return (
+          jsonFile.products.forEach((obj) => {
+            if (obj.name.toLowerCase().includes(this.state.inputTerm.toLowerCase()) &&
+            obj.type === this.state.inputType) {
+              results[obj.name] = obj.url;
+            }
+          });
+        }
+            // return (
 
-            jsonFile.products.filter((obj) =>
-            obj.name.toLowerCase().includes(this.state.inputTerm.toLowerCase()) &&
-            obj.type === this.state.inputType
-            )
-          );
-          }
+            // // jsonFile.products.filter((obj) =>
+            // // obj.name.toLowerCase().includes(this.state.inputTerm.toLowerCase()) &&
+            // // obj.type === this.state.inputType
+            // // )
+            
+            // );
+        return results;
   }
         
     
@@ -45,21 +62,29 @@ class AutoComplete extends React.Component {
     const results = this.matches();
     return (
       <div className='wrapper'>
-        <label>Search:</label>
-        <input onChange={(e) => this.handleInput(e)} value={this.state.inputTerm} />
-          <label>Type:</label>
-        <select onChange={(e) => this.setState({inputType: e.target.value})}>
-          <option value=''>All</option>
-          <option value='BANK'>Bank</option>
-          <option value='CREDIT_CARD'>Credit Card</option>
-          <option value='INVESTMENT'>Investment</option>
-          <option value='LOAN'>Loan</option>
-        </select>
+    
+
+  
+          <label>Type</label>
+
+          <select onChange={(e) => this.setState({inputType: e.target.value})}>
+            <option value=''>All</option>
+            <option value='BANK'>Bank</option>
+            <option value='CREDIT_CARD'>Credit Card</option>
+            <option value='INVESTMENT'>Investment</option>
+            <option value='LOAN'>Loan</option>
+          </select>
+          <label>Search</label>
+          <input className='search-term' onChange={(e) => this.handleInput(e)} 
+          value={this.state.inputTerm} placeholder='Type name here'/>
+    
         <ul className='search-results'>
 
-          {results ? results.map((entity, idx) => 
+          {results ? Object.keys(results).map((name, idx) => 
             <li key={idx}>
-              {entity.name}
+              <a href={results[name]}>
+              {name}
+              </a>
             </li>
           )
           : ""
