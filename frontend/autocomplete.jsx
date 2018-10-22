8758bd2e00b48abcc6a84469f877c53a7f9462f9
HN <hhnguyen92@gmail.com>
2018-10-22 13:36:36 -0700

    Implement type drop down

--- frontend/autocomplete.jsx
+++ frontend/autocomplete.jsx
@@ -4,21 +4,33 @@ import jsonFile from '../products.json';
 class AutoComplete extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {inputVal : ''};
+    this.state = {inputTerm : '', inputType: ''};
   }
 
 
   handleInput(e) {
-    this.setState({inputVal : e.target.value});
+    this.setState({inputTerm : e.target.value});
 
+  }
+
+  matches() {
+    //match names
+    //match type
+    //
   }
 
   render() {
     return (
       <div>
-        
-        
-        <input onChange={(e) => this.handleInput(e)} value={this.state.inputVal} />
+        <label>Search:</label>
+        <input onChange={(e) => this.handleInput(e)} value={this.state.inputTerm} />
+          <label>Type:</label>
+        <select onChange={(e) => this.setState({inputType: e.target.value})}>
+          <option value=''>All</option>
+          <option value='CREDIT_CARD'>Credit Card</option>
+          <option value='BANK'>Bank</option>
+          <option value='INVESTMENT'>Investment</option>
+        </select>
         <ul>
           {jsonFile.products.map((entity, idx) => 
             <li key={idx}>
